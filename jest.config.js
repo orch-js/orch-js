f6fc04286faf3fec1259cb7f05ed58f399e8d4d7
Runjuu <huang@runjuu.com>
2020-02-08 19:25:31 +0800

    dev: update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/?(*.)+(spec).ts?(x)'],
+  moduleNameMapper: {
+    "@orch/(.*)$": "<rootDir>/packages/$1/src/",
+  }
 }
