40f64d9f21a527d83a92288c20c4a914a9434317
Runjuu <huang@runjuu.com>
2020-02-09 16:16:55 +0800

    feat(model): enhance dispatcher’s type

---
+++ packages/model/__tests__/dispatcher.type.spec.ts
@@ -0,0 +1,34 @@
+import { assert, IsExact } from 'conditional-type-checks'
+
+import { Dispatcher } from '@orch/model/dispatchers/types'
+
+type ParameterLengthShouldBe<D extends Dispatcher<any>, L extends number> = IsExact<
+  Parameters<D>['length'],
+  L
+>
+type ParameterTypeShouldBe<D extends Dispatcher<any>, T> = IsExact<Parameters<D>[0], T>
+
+describe(`Dispatcher type`, () => {
+  it(`should only have one params for dispatcher`, () => {
+    assert<ParameterLengthShouldBe<Dispatcher<string>, 1>>(true)
+  })
+
+  it(`should return identical type`, () => {
+    assert<ParameterTypeShouldBe<Dispatcher<string>, string>>(true)
+    assert<ParameterTypeShouldBe<Dispatcher<number>, number>>(true)
+    assert<ParameterTypeShouldBe<Dispatcher<{ customType: number }>, { customType: number }>>(true)
+  })
+
+  it(`should return "any" type payload if type is "any"`, () => {
+    assert<ParameterLengthShouldBe<Dispatcher<any>, 1>>(true)
+    assert<ParameterTypeShouldBe<Dispatcher<any>, any>>(true)
+  })
+
+  it(`should return no payload if type is "void"`, () => {
+    assert<ParameterLengthShouldBe<Dispatcher<void>, 0>>(true)
+  })
+
+  it(`should return no payload if type is "unknown"`, () => {
+    assert<ParameterLengthShouldBe<Dispatcher<unknown>, 0>>(true)
+  })
+})
--- packages/model/src/dispatchers/types.ts
+++ packages/model/src/dispatchers/types.ts
@@ -3,15 +3,33 @@ import { Draft } from 'immer'
 
 import { ActionSymbol } from './symbols'
 
+// https://stackoverflow.com/questions/55541275/typescript-check-for-the-any-type
+type IfAny<T, Y, N> = 0 extends 1 & T ? Y : N
+
+type IsAny<T> = IfAny<T, true, false>
+
+type IsEmptyPayload<P> = P extends void
+  ? true
+  : IsAny<P> extends true
+  ? false
+  : unknown extends P
+  ? true
+  : false
+
 export type Action = {
   (): void
   identify: typeof ActionSymbol
 }
 
-export type Dispatcher<P> = {
-  (payload: P): void
-  asAction: (payload: P) => Action
-}
+export type Dispatcher<P> = IsEmptyPayload<P> extends true
+  ? {
+      (): void
+      asAction: () => Action
+    }
+  : {
+      (payload: P): void
+      asAction: (payload: P) => Action
+    }
 
 export type EffectFunc<P> = (payload$: Observable<P>) => Observable<Action>
 
