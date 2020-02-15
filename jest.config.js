9368c33291f84af1a8a29b85c3f0d76385f9f9d4
Runjuu <huang@runjuu.com>
2020-02-16 01:56:04 +0800

    dev(jest): update moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/?(*.)+(spec).ts?(x)'],
   moduleNameMapper: {
-    "@orch/(.*)$": "<rootDir>/packages/$1/src/",
-  }
+    '@orch/([^/]*)/(.*)$': '<rootDir>/packages/$1/src/$2',
+    '@orch/([^/]*)$': '<rootDir>/packages/$1/src/',
+  },
 }
