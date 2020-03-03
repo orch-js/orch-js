da051fca716195267ee67016b3b5b54f4f2663fd
Runjuu <huang@runjuu.com>
2020-03-03 23:30:40 +0800

    feat(react): store-context export `StoreProvider`

--- packages/react/src/store-context.ts
+++ packages/react/src/store-context.ts
@@ -2,3 +2,5 @@ import { createContext } from 'react'
 import { OrchStore } from '@orch/store'
 
 export const StoreContext = createContext(new OrchStore())
+
+export const StoreProvider = StoreContext.Provider
