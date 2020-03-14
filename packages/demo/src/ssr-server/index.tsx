5b3aa1c4e01b8968892effd5cbd5ae944e1ff77b
Runjuu <huang@runjuu.com>
2020-03-15 02:09:20 +0800

    chore: fix lint errors
    https://github.com/yannickcr/eslint-plugin-react/issues/2559

--- packages/demo/src/ssr-server/index.tsx
+++ packages/demo/src/ssr-server/index.tsx
@@ -32,6 +32,7 @@ app.get('/*', async (req, res) => {
 
   const renderedApp = renderToString(app)
 
+  // eslint-disable-next-line no-console
   console.log('========= \n', JSON.stringify(orchStore, null, 2))
 
   fs.readFile(indexHtmlFile, 'utf8', (err, data) => {
