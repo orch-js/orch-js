163d775181edcb542d2ae91ec8fe6a8c676265f6
Runjuu <huang@runjuu.com>
2020-06-20 21:31:10 +0800

    test(model/performers): performer

---
+++ packages/model/__tests__/performers/performer.spec.ts
@@ -0,0 +1,50 @@
+import { Observable } from 'rxjs'
+import { endWith, tap } from 'rxjs/operators'
+
+import { performer, disposePerformer } from '../../src/performers/performer'
+
+describe(`performers:performer`, () => {
+  it(`should subscribe returned observable`, () => {
+    const spy = jest.fn()
+
+    performer(() => {
+      return new Observable(() => {
+        spy()
+      })
+    })
+
+    expect(spy.mock.calls).toEqual([[]])
+  })
+
+  it(`should emit payload if trigger performer`, () => {
+    const spy = jest.fn()
+
+    const _performer = performer<number>((payload$) => payload$.pipe(tap(spy)))
+
+    _performer(44)
+
+    expect(spy.mock.calls).toEqual([[44]])
+  })
+
+  it(`should not emit payload if performer is disposed`, () => {
+    const spy = jest.fn()
+
+    const _performer = performer<number>((payload$) => payload$.pipe(tap(spy)))
+
+    disposePerformer(_performer)
+
+    _performer(44)
+
+    expect(spy.mock.calls).toEqual([])
+  })
+
+  it(`should complete payload$ after dispose performer`, () => {
+    const spy = jest.fn()
+
+    const _performer = performer<number>((payload$) => payload$.pipe(endWith('end'), tap(spy)))
+
+    disposePerformer(_performer)
+
+    expect(spy.mock.calls).toEqual([['end']])
+  })
+})
