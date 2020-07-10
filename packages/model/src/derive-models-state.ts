e2a18f19c8d34eca27e1508b313602fdd4dcd673
runjuu <huang@runjuu.com>
2020-07-10 22:37:41 +0800

    feat(model): update `derive-models-state` types

--- packages/model/src/derive-models-state.ts
+++ packages/model/src/derive-models-state.ts
@@ -1,24 +1,22 @@
-import { combineLatest } from 'rxjs'
+import { combineLatest, Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
 
-import type { OrchModel } from './orch-model'
 import { OrchState } from './orch-state'
 import { SetStateSymbol } from './const'
 
-export type StateOfModels<Ms extends OrchModel<any>[]> = {
-  [K in keyof Ms]: Ms[K] extends OrchModel<infer S> ? S : never
+export type DerivableModelType<S> = { state: { getState: () => S; state$: Observable<S> } }
+
+export type DerivableModels<Sn extends any[]> = {
+  [K in keyof Sn]: DerivableModelType<Sn[K]>
 }
 
-export function deriveModelsState<Ms extends OrchModel<any>[], S>(...models: Ms) {
-  return factory
-
-  function factory(f: (...states: StateOfModels<Ms>) => S): OrchState<S>
-  function factory(f: (...states: any[]) => any): OrchState<any> {
-    const defaultStates = models.map((model) => model.state.getState()) as StateOfModels<Ms>
-    const orchState = new OrchState<S>(f(...defaultStates))
+export function deriveModelsState<Sn extends any[]>(...models: DerivableModels<Sn>) {
+  return <R>(f: (...states: Sn) => R): OrchState<R> => {
+    const defaultStates = models.map((model) => model.state.getState()) as Sn
+    const orchState = new OrchState<R>(f(...defaultStates))
 
     const subscription = combineLatest(models.map((model) => model.state.state$))
-      .pipe(map((states) => f(...states)))
+      .pipe(map((states) => f(...(states as Sn))))
       .subscribe((newState) => orchState[SetStateSymbol](newState))
 
     orchState.state$.subscribe({ complete: () => subscription.unsubscribe() })
