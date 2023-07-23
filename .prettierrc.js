19443d60ddfc2bada351e8d04d85d4f02dfaec64
Runjuu <hi@runjuu.com>
2023-07-23 10:18:30 -0700

    dev: update prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  printWidth: 100,
+  semi: false,
+  trailingComma: 'all',
+  singleQuote: true,
+  plugins: [require.resolve('@ianvs/prettier-plugin-sort-imports')],
+  importOrder: [
+    '<BUILT_IN_MODULES>',
+    '',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^@orch/(.*)$',
+    '',
+    '^@/(.*)$',
+    '',
+    '^[./]',
+  ],
+}
