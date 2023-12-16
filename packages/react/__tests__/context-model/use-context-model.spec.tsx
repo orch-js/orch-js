6f402699cf103828d9823e4f8da6a3dac65e546c
runjuu <hi@runjuu.com>
2023-12-15 18:25:17 -0800

    feat(react/context-model): add selector support

--- packages/react/__tests__/context-model/use-context-model.spec.tsx
+++ packages/react/__tests__/context-model/use-context-model.spec.tsx
@@ -1,18 +1,23 @@
-import { renderHook } from '@testing-library/react-hooks'
+import { act, renderHook } from '@testing-library/react-hooks'
 import React from 'react'
 import { create } from 'react-test-renderer'
 import { describe, expect, it } from 'vitest'
 
-import { OrchModel } from '@orch/core'
+import { autoActivate, mutation, OrchModel } from '@orch/core'
 
 import { ContextModelProvider, useContextModel } from '../../src'
 
 type CountModelState = { count: number }
 
+@autoActivate()
 class CountModel extends OrchModel<CountModelState> {
   constructor(defaultState: CountModelState = { count: 0 }) {
     super(defaultState)
   }
+
+  setCount = mutation(this, (state, payload: number) => {
+    state.count = payload
+  })
 }
 
 describe(`useContextModel`, () => {
@@ -71,5 +76,58 @@ describe(`useContextModel`, () => {
     expect(getModelById('b')).toBe(modelB)
     expect(getModelById('c')).toBe(modelC)
     expect(getModelById('d')).toBe(modelD)
+  })
+
+  it(`should be able to use selector to get the state`, () => {
+    const model = new CountModel()
+    const { result } = renderHook(
+      () => useContextModel(CountModel, (state, model) => [state.count, model], []),
+      {
+        wrapper: ({ children }: React.PropsWithChildren) => (
+          <ContextModelProvider value={[model]}>{children}</ContextModelProvider>
+        ),
+      },
+    )
+
+    expect(result.current[0]).toBe(0)
+    expect(result.current[1]).toBe(model)
+  })
+
+  it(`should update state if changed`, () => {
+    const model = new CountModel()
+    const { result } = renderHook(() => useContextModel(CountModel, (state) => state.count, []), {
+      wrapper: ({ children }: React.PropsWithChildren) => (
+        <ContextModelProvider value={[model]}>{children}</ContextModelProvider>
+      ),
+    })
+
+    expect(result.current).toBe(0)
+
+    act(() => {
+      model.setCount(44)
+    })
+
+    expect(result.current).toBe(44)
+  })
+
+  it(`should be able to specific selector's deps`, () => {
+    type WrapperProps = { prefix: string; children?: React.ReactNode }
+    const model = new CountModel()
+    const { result, rerender } = renderHook(
+      ({ prefix }: { prefix: string }) =>
+        useContextModel(CountModel, (state) => `${prefix}: ${state.count}`, [prefix]),
+      {
+        initialProps: { prefix: 'a' },
+        wrapper: ({ children }: WrapperProps) => (
+          <ContextModelProvider value={[model]}>{children}</ContextModelProvider>
+        ),
+      },
+    )
+
+    expect(result.current).toBe('a: 0')
+
+    rerender({ prefix: 'b' })
+
+    expect(result.current).toBe('b: 0')
   })
 })
--- packages/react/src/context-model/use-context-model.ts
+++ packages/react/src/context-model/use-context-model.ts
@@ -1,15 +1,35 @@
 import React from 'react'
 
-import { ConstructorType, OrchModel } from '@orch/core'
+import { ConstructorType, OrchModel, OrchModelState } from '@orch/core'
 
+import { useModelState } from '../hooks/use-model-state'
 import { ContextModelContext } from './context-model-context'
 
-export function useContextModel<M extends OrchModel<any>>(ModelClass: ConstructorType<M>): M {
+export function useContextModel<M extends OrchModel<any>, S>(
+  ModelClass: ConstructorType<M>,
+  selector: (state: OrchModelState<M>, model: M) => S,
+  deps: React.DependencyList,
+): S
+
+export function useContextModel<M extends OrchModel<any>>(ModelClass: ConstructorType<M>): M
+
+export function useContextModel<M extends OrchModel<any>, S>(
+  ModelClass: ConstructorType<M>,
+  selector?: (state: OrchModelState<M>, model: M) => S,
+  deps: React.DependencyList = [],
+): S | M {
   const context = React.useContext(ContextModelContext)
-  const model = React.useMemo(() => context.get(ModelClass), [context])
+  const model = React.useMemo(
+    () => context.get(ModelClass) as OrchModelState<M>,
+    [context, ModelClass],
+  )
 
   if (model) {
-    return model as M
+    if (selector) {
+      return useModelState(model, selector, deps)
+    } else {
+      return model as M
+    }
   } else {
     throw new Error(`There is no instance for ${ModelClass}`)
   }
